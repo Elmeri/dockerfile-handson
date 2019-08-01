47b47b0a654609e475c8540951270d977cac9033
Elmeri Poikolainen <elmeri.poikolainen@eficode.com>
2019-08-01 11:37:40 +0300

    added css loading + main.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash'
 
+require('./main.css');
+
 function component() {
+    let outer_element = document.createElement('div')
     let element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'world'], ' ');
+    element.innerHTML = _.join(['Hello', 'docker'], ' ');
 
-    return element;
+    element.className = "inner"
+    outer_element.className = "outer"
+    outer_element.appendChild(element)
+
+    return outer_element;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -21,5 +21,13 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules:[
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+    ]
   }
 };
